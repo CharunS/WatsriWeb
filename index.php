9da8d0621f812ec9757c92cdfacfd044c90d7ac9
Charun Saenbunsiri <charun.s@frontiscompany.com>
2019-11-17 23:17:05 +0700

    first commit

---
+++ index.php
@@ -0,0 +1,318 @@
+<!DOCTYPE html>
+<html>
+<head>
+    <meta charset="utf-8" />
+    <meta name="viewport" content="width=device-width, initial-scale=1, shrink-to-fit=no">
+    
+    <!-- Custom Meta -->
+    <meta http-equiv="Content-Type" content="text/html; charset=utf8" />
+	  <meta name="keywords" content="วัดศรีสุดาราม,โรงเรียนวัดศรีสุดาราม,วัดศรี,สุดาราม,watsrisudaramschool,watsrisudaram" />
+	  <meta name="description" content="Watsrisudaram School, โรงเรียนวัดศรีสุดาราม" />
+
+    <title>โรงเรียนวัดศรีสุดาราม - Watsrisudaram Shcool</title>
+    
+    <!-- Tab icon -->
+    <link rel="icon" href="img/tab_icon.png">
+
+    <!-- Bootstrap core CSS -->
+    <link href="Extensions/vendor/bootstrap/css/bootstrap.min.css" rel="stylesheet">
+    <!-- Custom styles for this template -->
+    <link href="Extensions/css/modern-business.css" rel="stylesheet">
+    <!-- Custom Css-->
+    <link href="Extensions/vendor/bootstrap/css/CustomCss.css" rel="stylesheet">
+    <!-- font Awnsome-->
+    <link href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/4.7.0/css/font-awesome.min.css" rel="stylesheet">
+    
+</head>
+    <body>
+    <!-- Navigation -->
+    <nav class="navbar fixed-top navbar-expand-lg navbar-dark bg-dark fixed-top">
+        <div class="container">
+            <!-- <a class="navbar-brand" href="#">โรงเรียนวัดศรีสุดาราม</a> -->
+            <img src="Img/logo.png" width="5%" height="5%">
+            <button class="navbar-toggler navbar-toggler-right" type="button" data-toggle="collapse" data-target="#navbarResponsive" aria-controls="navbarResponsive" aria-expanded="false" aria-label="Toggle navigation">
+                <span class="navbar-toggler-icon"></span>
+            </button>
+            <div class="collapse navbar-collapse" id="navbarResponsive">
+                <ul class="navbar-nav ml-auto">
+                <li class="nav-item">
+                    <a class="nav-link" href="#">หน้าแรก</a>
+                </li>
+                <li class="nav-item dropdown">
+                    <a class="nav-link dropdown-toggle" href="#" id="navbarDropdownBlog" data-toggle="dropdown" aria-haspopup="true" aria-expanded="false">
+                    เกี่ยวกับโรงเรียน
+                    </a>
+                    <div class="dropdown-menu dropdown-menu-right" aria-labelledby="navbarDropdownBlog">
+                    <a class="dropdown-item" href="page/History.php">ประวัติโรงเรียน</a>
+                    <a class="dropdown-item" href="#">สัญลักษณ์โรงเรียน</a>
+                    <a class="dropdown-item" href="#">วิสัยทัศน์ / ปรัชญา</a>
+                    <a class="dropdown-item" href="#">พันธกิจ / เป้าหมาย</a>
+                    </div>
+                </li>
+                <li class="nav-item dropdown">
+                    <a class="nav-link dropdown-toggle" href="#" id="navbarDropdownPortfolio" data-toggle="dropdown" aria-haspopup="true" aria-expanded="false">
+                    บุคลากร
+                    </a>
+                    <div class="dropdown-menu dropdown-menu-right" aria-labelledby="navbarDropdownPortfolio">
+                    <a class="dropdown-item" href="#">คณะผู้บริหารและบุคลากร</a>
+                    <a class="dropdown-item" href="#">กลุ่มสาระฯภาษาไทย</a>
+                    <a class="dropdown-item" href="#">กลุ่มสาระฯคณิตศาสตร์</a>
+                    <a class="dropdown-item" href="#">กลุ่มสาระฯวิทยาศาสตร์</a>
+                    <a class="dropdown-item" href="#">กลุ่มสาระฯสังคมศึกษาฯ</a>
+                    <a class="dropdown-item" href="#">กลุ่มสาระฯสุขศึกษาฯ</a>
+                    <a class="dropdown-item" href="#">กลุ่มสาระฯศิลปะ</a>
+                    <a class="dropdown-item" href="#">กลุ่มสาระฯการงานอาชีพฯ</a>
+                    <a class="dropdown-item" href="#">กลุ่มสาระฯภาษาต่างประเทศ</a>
+                    <a class="dropdown-item" href="#">ปฐมวัย</a>
+                    </div>
+                </li>
+                <li class="nav-item">
+                    <a class="nav-link" href="#">ปฏิทินกิจกรรม</a>
+                </li>
+                </ul>
+            </div>
+        </div>
+    </nav>
+<header>
+
+    <div id="carouselExampleIndicators" class="carousel slide" data-ride="carousel">
+      <ol class="carousel-indicators">
+        <li data-target="#carouselExampleIndicators" data-slide-to="0" class="active"></li>
+        <li data-target="#carouselExampleIndicators" data-slide-to="1"></li>
+        <li data-target="#carouselExampleIndicators" data-slide-to="2"></li>
+        <li data-target="#carouselExampleIndicators" data-slide-to="3"></li>
+        <li data-target="#carouselExampleIndicators" data-slide-to="4"></li>
+      </ol>
+      <div class="carousel-inner" role="listbox">
+        <!-- Slide 1 - Set the background image for this slide in the line below -->
+        <div class="carousel-item active" style="background-image: url('Img/IndexSlide/1.png')">
+          <div class="carousel-caption d-none d-md-block">
+            <!-- <h3>First Slide</h3>
+            <p>This is a description for the first slide.</p> -->
+          </div>
+        </div>
+        <!-- Slide 2 - Set the background image for this slide in the line below -->
+        <div class="carousel-item" style="background-image: url('Img/IndexSlide/2.png')">
+          <div class="carousel-caption d-none d-md-block">
+          </div>
+        </div>
+        <!-- Slide 3 - Set the background image for this slide in the line below -->
+        <div class="carousel-item" style="background-image: url('Img/IndexSlide/2.png')">
+          <div class="carousel-caption d-none d-md-block">
+          </div>
+        </div>
+        <!-- Slide 4 - Set the background image for this slide in the line below -->
+        <div class="carousel-item" style="background-image: url('Img/IndexSlide/3.png')">
+          <div class="carousel-caption d-none d-md-block">
+          </div>
+        </div>
+        <!-- Slide 5 - Set the background image for this slide in the line below -->
+        <div class="carousel-item" style="background-image: url('Img/IndexSlide/4.png')">
+          <div class="carousel-caption d-none d-md-block">
+          </div>
+        </div>
+      </div>
+      <a class="carousel-control-prev" href="#carouselExampleIndicators" role="button" data-slide="prev">
+        <span class="carousel-control-prev-icon" aria-hidden="true"></span>
+        <span class="sr-only">Previous</span>
+      </a>
+      <a class="carousel-control-next" href="#carouselExampleIndicators" role="button" data-slide="next">
+        <span class="carousel-control-next-icon" aria-hidden="true"></span>
+        <span class="sr-only">Next</span>
+      </a>
+    </div>
+</header>
+
+    <!-- Page Content -->
+    <div class="container">
+
+    <!-- <div class="row">
+      <div class="col-lg-2 mb-2" style="margin-top: .5rem!important;padding-right: 0 !important;">
+        <img src="Img/logo.png" width="80%" height="100%">
+      </div>
+      <div class="col-lg-8 mb-8" style="padding-left: 0 !important;">
+        <h1 class="my-4">โรงเรียนวัดศรีสุดาราม</h1>
+      </div>
+    </div> -->
+  
+    <!-- Marketing Icons Section -->
+    <!-- <div class="row">
+      <div class="col-lg-4 mb-4">
+        <div class="card h-100">
+          <h4 class="card-header">Card Title</h4>
+          <div class="card-body">
+            <p class="card-text">Lorem ipsum dolor sit amet, consectetur adipisicing elit. Sapiente esse necessitatibus neque.</p>
+          </div>
+          <div class="card-footer">
+            <a href="#" class="btn btn-primary">Learn More</a>
+          </div>
+        </div>
+      </div>
+      <div class="col-lg-4 mb-4">
+        <div class="card h-100">
+          <h4 class="card-header">Card Title</h4>
+          <div class="card-body">
+            <p class="card-text">Lorem ipsum dolor sit amet, consectetur adipisicing elit. Reiciendis ipsam eos, nam perspiciatis natus commodi similique totam consectetur praesentium molestiae atque exercitationem ut consequuntur, sed eveniet, magni nostrum sint fuga.</p>
+          </div>
+          <div class="card-footer">
+            <a href="#" class="btn btn-primary">Learn More</a>
+          </div>
+        </div>
+      </div>
+      <div class="col-lg-4 mb-4">
+        <div class="card h-100">
+          <h4 class="card-header">Card Title</h4>
+          <div class="card-body">
+            <p class="card-text">Lorem ipsum dolor sit amet, consectetur adipisicing elit. Sapiente esse necessitatibus neque.</p>
+          </div>
+          <div class="card-footer">
+            <a href="#" class="btn btn-primary">Learn More</a>
+          </div>
+        </div>
+      </div>
+    </div> -->
+    <!-- /.row -->
+
+    <!-- Portfolio Section -->
+    <h2>ข่าวประชาสัมพันธ์</h2>
+    <hr>
+    <div class="row">
+
+      <div class="col-lg-4 col-sm-6 portfolio-item">
+        <div class="card h-100">
+          <a href="#"><img class="card-img-top" src="http://placehold.it/700x400" alt=""></a>
+          <div class="card-body">
+            <h4 class="card-title">
+              <a href="#">Project One</a>
+            </h4>
+            <p class="card-text">Lorem ipsum dolor sit amet, consectetur adipisicing elit. Amet numquam aspernatur eum quasi sapiente nesciunt? Voluptatibus sit, repellat sequi itaque deserunt, dolores in, nesciunt, illum tempora ex quae? Nihil, dolorem!</p>
+          </div>
+        </div>
+      </div>
+      <div class="col-lg-4 col-sm-6 portfolio-item">
+        <div class="card h-100">
+          <a href="#"><img class="card-img-top" src="http://placehold.it/700x400" alt=""></a>
+          <div class="card-body">
+            <h4 class="card-title">
+              <a href="#">Project Two</a>
+            </h4>
+            <p class="card-text">Lorem ipsum dolor sit amet, consectetur adipiscing elit. Nam viverra euismod odio, gravida pellentesque urna varius vitae.</p>
+          </div>
+        </div>
+      </div>
+      <div class="col-lg-4 col-sm-6 portfolio-item">
+        <div class="card h-100">
+          <a href="#"><img class="card-img-top" src="http://placehold.it/700x400" alt=""></a>
+          <div class="card-body">
+            <h4 class="card-title">
+              <a href="#">Project Three</a>
+            </h4>
+            <p class="card-text">Lorem ipsum dolor sit amet, consectetur adipisicing elit. Quos quisquam, error quod sed cumque, odio distinctio velit nostrum temporibus necessitatibus et facere atque iure perspiciatis mollitia recusandae vero vel quam!</p>
+          </div>
+        </div>
+      </div>
+      <div class="col-lg-4 col-sm-6 portfolio-item">
+        <div class="card h-100">
+          <a href="#"><img class="card-img-top" src="http://placehold.it/700x400" alt=""></a>
+          <div class="card-body">
+            <h4 class="card-title">
+              <a href="#">Project Four</a>
+            </h4>
+            <p class="card-text">Lorem ipsum dolor sit amet, consectetur adipiscing elit. Nam viverra euismod odio, gravida pellentesque urna varius vitae.</p>
+          </div>
+        </div>
+      </div>
+      <div class="col-lg-4 col-sm-6 portfolio-item">
+        <div class="card h-100">
+          <a href="#"><img class="card-img-top" src="http://placehold.it/700x400" alt=""></a>
+          <div class="card-body">
+            <h4 class="card-title">
+              <a href="#">Project Five</a>
+            </h4>
+            <p class="card-text">Lorem ipsum dolor sit amet, consectetur adipiscing elit. Nam viverra euismod odio, gravida pellentesque urna varius vitae.</p>
+          </div>
+        </div>
+      </div>
+      <div class="col-lg-4 col-sm-6 portfolio-item">
+        <div class="card h-100">
+          <a href="#"><img class="card-img-top" src="http://placehold.it/700x400" alt=""></a>
+          <div class="card-body">
+            <h4 class="card-title">
+              <a href="#">Project Six</a>
+            </h4>
+            <p class="card-text">Lorem ipsum dolor sit amet, consectetur adipisicing elit. Itaque earum nostrum suscipit ducimus nihil provident, perferendis rem illo, voluptate atque, sit eius in voluptates, nemo repellat fugiat excepturi! Nemo, esse.</p>
+          </div>
+        </div>
+      </div>
+    </div>
+    <!-- /.row -->
+
+    <!-- Call to Action Section -->
+    <div class="row mb-4">
+      <div class="col-md-8">
+        <p>หากต้องการดูข่าวสารเพิ่มเติมกรุณาคลิกที่ปุ่มดูข่าวทั้งหมด</p>
+      </div>
+      <div class="col-md-4">
+        <a class="btn btn-lg btn-secondary btn-block" href="#"></span> ข่าวทั้งหมด</a>
+      </div>
+    </div>
+
+    <!-- Features Section -->
+    <div class="row">
+      <div class="col-lg-8">
+        <iframe src="https://www.google.com/maps/embed?pb=!1m18!1m12!1m3!1d4806.894161398964!2d100.47006617150295!3d13.76665328115913!2m3!1f0!2f0!3f0!3m2!1i1024!2i768!4f13.1!3m3!1m2!1s0x30e29990bb60e651%3A0x5e1f85d7d54289a9!2z4LmC4Lij4LiH4LmA4Lij4Li14Lii4LiZ4Lin4Lix4LiU4Lio4Lij4Li14Liq4Li44LiU4Liy4Lij4Liy4Lih!5e0!3m2!1sth!2sth!4v1562479932001!5m2!1sth!2sth" width="100%" height="500" frameborder="0" style="border:0" allowfullscreen></iframe>
+      </div>
+      <div class="col-lg-4">
+        <!-- List line : You can add new link here. -->
+        <div class="col-lg-12">
+          <a href="https://www.facebook.com/Watsrisudaram/" target="_blank" class="fa fa-facebook" style="width : 60%;height : 60%; display: block; margin-left: auto; margin-right: auto; padding-bottom: 10px;"></a>
+        </div>
+        <div class="col-lg-12">			
+          <img src="Img/DocDownload.png" style="width : 60%;height : 60%; display: block; margin-left: auto; margin-right: auto; padding-bottom: 10px;">
+        </div>
+
+        <div class="col-lg-12">		
+          <a href="http://www.bangkok.go.th/bangkokeducation">
+            <img src="Img/ED_Bangkok.jpg" style="width : 60%;height : 60%; display: block; margin-left: auto; margin-right: auto; padding-bottom: 10px;">
+          </a>
+        </div>
+
+        <div class="col-lg-12">
+          <a href="https://www.niets.or.th/th/">
+            <img src="Img/NIETS.jpg" style="width : 60%;height : 60%; display: block; margin-left: auto; margin-right: auto; padding-bottom: 10px;"> 
+          </a>
+        </div>
+
+        <div class="col-lg-12">
+          <a href="https://www.eduzones.com">
+            <img src="Img/Eduzones.jpg" style="width : 60%;height : 60%; display: block; margin-left: auto; margin-right: auto; padding-bottom: 10px;">
+          </a>
+        </div>
+
+        <div class="col-lg-12">
+          <a href="http://www.moe.go.th/moe/th/home/">
+				    <img src="Img/MFD.png" style="width : 60%;height : 60%; display: block; margin-left: auto; margin-right: auto; padding-bottom: 10px;">
+          </a>
+        </div>
+
+      </div>
+    </div>
+    <!-- /.row -->
+
+  </div>
+  <!-- /.container -->
+
+    <!-- Footer -->
+    <footer class="py-5 bg-dark">
+        <div class="container">
+            <p class="m-0 text-center text-white">Copyright &copy; โรงเรียนวัดศรีสุดาราม ที่อยู่ 54/8 ซอยวัดศรีสุดาราม แขวงบางขุนนนท์ เขตบางกอกน้อย กรุงเทพมหานคร 10700 โทร.0-2424-0424</p>
+        </div>
+        <!-- /.container -->
+    </footer>
+        
+    <!-- Bootstrap core JavaScript -->
+    <script src="Extensions/vendor/jquery/jquery.min.js"></script>
+    <script src="Extensions/vendor/bootstrap/js/bootstrap.bundle.min.js"></script>
+
+    </body>
+</html>
---
+++ page/History.php
@@ -0,0 +1,180 @@
+<!DOCTYPE html>
+<html lang="en">
+
+  <head>
+
+    <meta charset="utf-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1, shrink-to-fit=no">
+    <meta name="description" content="">
+    <meta name="author" content="">
+
+    <title>Modern Business - Start Bootstrap Template</title>
+
+    <!-- Tab icon -->
+    <link rel="icon" href="../img/tab_icon.png">
+
+    <!-- Bootstrap core CSS -->
+    <link href="../Extensions/vendor/bootstrap/css/bootstrap.min.css" rel="stylesheet">
+    <!-- Custom styles for this template -->
+    <link href="../Extensions/css/modern-business.css" rel="stylesheet">
+    <!-- Custom Css-->
+    <link href="../Extensions/vendor/bootstrap/css/CustomCss.css" rel="stylesheet">
+    <!-- font Awnsome-->
+    <link href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/4.7.0/css/font-awesome.min.css" rel="stylesheet">
+
+  </head>
+
+  <body>
+
+    <!-- Navigation -->
+    <nav class="navbar fixed-top navbar-expand-lg navbar-dark bg-dark fixed-top">
+      <div class="container">
+          <!-- <a class="navbar-brand" href="#">โรงเรียนวัดศรีสุดาราม</a> -->
+          <img src="../Img/logo.png" width="5%" height="5%">
+          <button class="navbar-toggler navbar-toggler-right" type="button" data-toggle="collapse" data-target="#navbarResponsive" aria-controls="navbarResponsive" aria-expanded="false" aria-label="Toggle navigation">
+              <span class="navbar-toggler-icon"></span>
+          </button>
+          <div class="collapse navbar-collapse" id="navbarResponsive">
+              <ul class="navbar-nav ml-auto">
+              <li class="nav-item">
+                  <a class="nav-link" href="../index.php">หน้าแรก</a>
+              </li>
+              <li class="nav-item dropdown">
+                  <a class="nav-link dropdown-toggle" href="#" id="navbarDropdownBlog" data-toggle="dropdown" aria-haspopup="true" aria-expanded="false">
+                  เกี่ยวกับโรงเรียน
+                  </a>
+                  <div class="dropdown-menu dropdown-menu-right" aria-labelledby="navbarDropdownBlog">
+                  <a class="dropdown-item" href="#">ประวัติโรงเรียน</a>
+                  <a class="dropdown-item" href="#">สัญลักษณ์โรงเรียน</a>
+                  <a class="dropdown-item" href="#">วิสัยทัศน์ / ปรัชญา</a>
+                  <a class="dropdown-item" href="#">พันธกิจ / เป้าหมาย</a>
+                  </div>
+              </li>
+              <li class="nav-item dropdown">
+                  <a class="nav-link dropdown-toggle" href="#" id="navbarDropdownPortfolio" data-toggle="dropdown" aria-haspopup="true" aria-expanded="false">
+                  บุคลากร
+                  </a>
+                  <div class="dropdown-menu dropdown-menu-right" aria-labelledby="navbarDropdownPortfolio">
+                  <a class="dropdown-item" href="#">คณะผู้บริหารและบุคลากร</a>
+                  <a class="dropdown-item" href="#">กลุ่มสาระฯภาษาไทย</a>
+                  <a class="dropdown-item" href="#">กลุ่มสาระฯคณิตศาสตร์</a>
+                  <a class="dropdown-item" href="#">กลุ่มสาระฯวิทยาศาสตร์</a>
+                  <a class="dropdown-item" href="#">กลุ่มสาระฯสังคมศึกษาฯ</a>
+                  <a class="dropdown-item" href="#">กลุ่มสาระฯสุขศึกษาฯ</a>
+                  <a class="dropdown-item" href="#">กลุ่มสาระฯศิลปะ</a>
+                  <a class="dropdown-item" href="#">กลุ่มสาระฯการงานอาชีพฯ</a>
+                  <a class="dropdown-item" href="#">กลุ่มสาระฯภาษาต่างประเทศ</a>
+                  <a class="dropdown-item" href="#">ปฐมวัย</a>
+                  </div>
+              </li>
+              <li class="nav-item">
+                  <a class="nav-link" href="#">ปฏิทินกิจกรรม</a>
+              </li>
+              </ul>
+          </div>
+      </div>
+    </nav>
+
+    <!-- Page Content -->
+    <div class="container">
+
+      <!-- Page Heading/Breadcrumbs -->
+      <h1 class="mt-4 mb-3">ประวัติโรงเรียนวัดศรีสุดาราม
+        <!-- <small>โรงเรียนวัดศรีสุดาราม</small> -->
+      </h1>
+
+      <ol class="breadcrumb">
+        <li class="breadcrumb-item">
+          <a href="../index.php">หน้าแรก</a>
+        </li>
+        <li class="breadcrumb-item active">ประวัติโรงเรียน</li>
+      </ol>
+
+      <!-- Intro Content -->
+      <div class="row">
+        <div class="col-lg-6">
+          <img class="img-fluid rounded mb-4" src="http://placehold.it/750x450" alt="">
+        </div>
+        <div class="col-lg-6">
+          <h2>โรงเรียนวัดศรีสุดาราม</h2>
+          <p>โรงเรียนวัดศรีสุดารามเปิดทำการสอนตั้งแต่ปี พ.ศ.2476 เป็นโรงเรียนประชาบาล สังกัดกระทรวงศึกษาธิการ โดยอาศัยศาลาการเปรียญของวัดศรีสุดาราม (ชีปะขาว) ริมคลองบางกอกน้อย ทำการสอนเริ่มชั้นประถมศึกษาปีที่ 1 และขยายชั้นเรียนจนถึงชั้นประถมศึกษาปีที่ 4 มีขุนสุนทรสิธ เป็นครูใหญ่คนแรก</p>
+          <p>พ.ศ.2485 โรงเรียนได้โอนมาขึ้นกับเทศบาลธนบุรี</p>
+          <p>พ.ศ.2490 โรงเรียนโอนไปขึ้นกับกรมสามัญ กระทรวงศึกษาธิการ</p>
+          <p>พ.ศ.2504 ได้โอนครูประชาบาลในเขตเทศบาล กลับมาขึ้นกับเทศบาลตามเดิมอีก ต่อมาทางกรุงเทพมหานครได้รวมโรงเรียนในจังหวัดพระนครและจังหวัดธนบุรี จำนวนกว่า 400 โรงเรียน มาอยู่ในสังกัดกรุงเทพมหานคร รวมทั้งโรงเรียนวัดศรีสุดารามด้วย</p>
+          <p>พ.ศ.2528 จัดงานวันเด็กสมทบทุนมูลนิธิครบ 1 แสนบาทถ้วน จัดตั้งมูลนิธิศรีสุดาราม ขอจดทะเบียนต่อสภาวัฒนธรรมแห่งชาติ ได้รับอนุมัติเมื่อวันที่ 8 มีนาคม 2528 เลขที่ 3.169/2528</p>
+          <p>ปัจจุบันโรงเรียนวัดศรีสุดารามตั้งอยู่เลขที่ 54/8 ซอยวัดศรีสุดาราม แขวงบางขุนนนท์ เขตบางกอกน้อย กรุงเทพมหานคร 10700</p>
+        </div>
+      </div>
+      <!-- /.row -->
+
+      <!-- Team Members -->
+      <h2>บรรยากาศภายในโรงเรียน</h2>
+
+      <div class="row">
+        <div class="col-lg-4 mb-4">
+          <div class="card text-center">
+            <img class="card-img-top" src="http://placehold.it/750x450" alt="">
+            <div class="card-body">
+              <h4 class="card-title">Team Member</h4>
+            </div>
+          </div>
+        </div>
+        <div class="col-lg-4 mb-4">
+          <div class="card text-center">
+            <img class="card-img-top" src="http://placehold.it/750x450" alt="">
+            <div class="card-body">
+              <h4 class="card-title">Team Member</h4>
+            </div>
+          </div>
+        </div>
+        <div class="col-lg-4 mb-4">
+          <div class="card text-center">
+            <img class="card-img-top" src="http://placehold.it/750x450" alt="">
+            <div class="card-body">
+              <h4 class="card-title">Team Member</h4>
+            </div>
+          </div>
+        </div>
+      </div>
+      <!-- /.row -->
+
+      <div class="row">
+        <div class="col-lg-2 col-sm-4 mb-4">
+          <img class="img-fluid" src="http://placehold.it/500x300" alt="">
+        </div>
+        <div class="col-lg-2 col-sm-4 mb-4">
+          <img class="img-fluid" src="http://placehold.it/500x300" alt="">
+        </div>
+        <div class="col-lg-2 col-sm-4 mb-4">
+          <img class="img-fluid" src="http://placehold.it/500x300" alt="">
+        </div>
+        <div class="col-lg-2 col-sm-4 mb-4">
+          <img class="img-fluid" src="http://placehold.it/500x300" alt="">
+        </div>
+        <div class="col-lg-2 col-sm-4 mb-4">
+          <img class="img-fluid" src="http://placehold.it/500x300" alt="">
+        </div>
+        <div class="col-lg-2 col-sm-4 mb-4">
+          <img class="img-fluid" src="http://placehold.it/500x300" alt="">
+        </div>
+      </div>
+      <!-- /.row -->
+
+    </div>
+    <!-- /.container -->
+
+    <!-- Footer -->
+    <footer class="py-5 bg-dark">
+      <div class="container">
+          <p class="m-0 text-center text-white">Copyright &copy; โรงเรียนวัดศรีสุดาราม ที่อยู่ 54/8 ซอยวัดศรีสุดาราม แขวงบางขุนนนท์ เขตบางกอกน้อย กรุงเทพมหานคร 10700 โทร.0-2424-0424</p>
+      </div>
+      <!-- /.container -->
+    </footer>
+          
+    <!-- Bootstrap core JavaScript -->
+    <script src="Extensions/vendor/jquery/jquery.min.js"></script>
+    <script src="Extensions/vendor/bootstrap/js/bootstrap.bundle.min.js"></script>
+
+  </body>
+
+</html>
